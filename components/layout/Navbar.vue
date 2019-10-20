1bc9d1d2b1a31f0975d11b77f21e980a9d9c87ee
phr3nzy <adilosama47@gmail.com>
2019-10-20 21:53:07 +0200

    Edit: navbar icons

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,51 +1,51 @@
 <template>
-  <div>
-    <b-navbar class="white" toggleable="lg" variant="info">
-      <b-navbar-brand to="/">
-        <b-img
-          class="mr-2"
-          height="40px"
-          src="icon-40x40.png"
-          alt="Dawai Logo"
-          aria-label="Dawai Logo Picture"
-        ></b-img
-        >Dawai</b-navbar-brand
-      >
+  <b-navbar type="light" variant="white" toggleable="lg">
+    <b-navbar-brand to="/">
+      <b-img
+        class="mr-2"
+        height="40px"
+        src="icon-40x40.png"
+        alt="Dawai Logo"
+        aria-label="Dawai Logo Picture"
+      ></b-img
+      >Dawai</b-navbar-brand
+    >
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item active-class="accent--text" to="/#about"
-            >About</b-nav-item
-          >
-          <b-nav-item active-class="accent--text" to="/#donate"
-            >Donate</b-nav-item
-          >
-          <b-nav-item active-class="accent--text" to="/#features"
-            >Features</b-nav-item
-          >
-          <b-nav-item active-class="accent--text" to="/#how-it-works"
-            >How It Works</b-nav-item
-          >
-          <b-nav-item active-class="accent--text" to="/#pricing"
-            >Pricing</b-nav-item
-          >
-          <b-nav-item active-class="accent--text" to="/#contact"
-            >Contact Us</b-nav-item
-          >
-        </b-navbar-nav>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item active-class="accent--text" to="/#about"
+          ><font-awesome-icon :icon="['fas', 'info']" /> About</b-nav-item
+        >
+        <b-nav-item active-class="accent--text" to="/#donate"
+          ><font-awesome-icon :icon="['fas', 'donate']" /> Donate</b-nav-item
+        >
+        <b-nav-item active-class="accent--text" to="/#features"
+          ><font-awesome-icon :icon="['fas', 'star']" /> Features</b-nav-item
+        >
+        <b-nav-item active-class="accent--text" to="/#how-it-works"
+          ><font-awesome-icon :icon="['fas', 'cogs']" /> How It
+          Works</b-nav-item
+        >
+        <b-nav-item active-class="accent--text" to="/#pricing"
+          ><font-awesome-icon :icon="['fas', 'coins']" /> Pricing</b-nav-item
+        >
+        <b-nav-item active-class="accent--text" to="/#contact"
+          ><font-awesome-icon :icon="['fas', 'envelope']" /> Contact
+          Us</b-nav-item
+        >
+      </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <!-- <b-navbar-nav class="ml-auto">
-          <b-button class="secondary my-1 mx-2 shadow" pill
-            >We accept Donations <font-awesome-icon :icon="['fas', 'donate']"
-          /></b-button>
-          <b-button class="github-black my-1 mx-2 shadow" pill
-            >GitHub Repository <font-awesome-icon :icon="['fab', 'github']"
-          /></b-button>
-        </b-navbar-nav> -->
-      </b-collapse>
-    </b-navbar>
-  </div>
+      <!-- Right aligned nav items -->
+      <!-- <b-navbar-nav class="ml-auto">
+        <b-button class="secondary my-1 mx-2 shadow" pill
+          >We accept Donations <font-awesome-icon :icon="['fas', 'donate']"
+        /></b-button>
+        <b-button class="github-black my-1 mx-2 shadow" pill
+          >GitHub Repository <font-awesome-icon :icon="['fab', 'github']"
+        /></b-button>
+      </b-navbar-nav> -->
+    </b-collapse>
+  </b-navbar>
 </template>
