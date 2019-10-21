18f8a5253b622339beb8d0cc927e25f9d5d8b6ec
phr3nzy <adilosama47@gmail.com>
2019-10-21 11:51:37 +0200

    Edit: add how it works section content

--- components/index/HowItWorksSection.vue
+++ components/index/HowItWorksSection.vue
@@ -5,5 +5,44 @@
         <h2 class="text-center my-5">How It Works</h2>
       </b-col>
     </b-row>
+    <b-row class="align-items-center justify-content-center my-4">
+      <b-col class="mb-3" lg="6" sm="12">
+        <b-img src="/pharmacy-management.png" fluid></b-img>
+      </b-col>
+      <b-col class="my-3" lg="4" sm="12">
+        <p class="lead">
+          Pharmacies will manage their inventory and declare the availability of
+          their medicine
+        </p>
+        <b-button class="btn-accent shadow" size="lg" pill
+          >See it in Action!</b-button
+        >
+      </b-col>
+    </b-row>
+    <b-row class="align-items-center justify-content-center my-4">
+      <b-col class="mb-3" lg="6" sm="12">
+        <b-img src="/pharmacy-management.png" fluid></b-img>
+      </b-col>
+      <b-col class="my-3" lg="4" sm="12">
+        <p class="lead">
+          Patients will search for the medicine and automatically be connected
+          to the pharmacies’ inventory
+        </p>
+        <b-button class="btn-accent shadow" size="lg" pill
+          >Try it Now!</b-button
+        >
+      </b-col>
+    </b-row>
+    <b-row class="align-items-center justify-content-center my-4">
+      <b-col class="mb-3" lg="6" sm="12">
+        <b-img src="/map.png" fluid></b-img>
+      </b-col>
+      <b-col class="my-3" lg="4" sm="12">
+        <p class="lead">
+          Patients will be able to find the nearest pharmacy and get directions
+          on a Map
+        </p></b-col
+      >
+    </b-row>
   </section>
 </template>
