6debd84b3fff0310616b8d9065778428910bc80e
phr3nzy <adilosama47@gmail.com>
2019-10-22 12:35:22 +0200

    Edit: center align footer text

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer-classic mt-5">
     <b-container class="py-4">
-      <b-row class="row align-items-center justify-content-center">
+      <b-row class="row align-items-center justify-content-center text-center">
         <p>
           Copyright © <span class="copyright-year">2019</span><span> </span
           ><span> All Rights Reserved</span><span> | </span>
